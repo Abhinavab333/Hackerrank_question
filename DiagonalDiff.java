d5a6689edb55ce66b4b763f5ccbd39ea90c4d172
avabhi <32228794+avabhi@users.noreply.github.com>
2018-03-20 12:13:24 +0530

    New Problem
    Diagonal Difference

---
+++ DiagonalDiff.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    /*
+     * Complete the diagonalDifference function below.
+     */
+    static int diagonalDifference(int[][] a) {
+        /*
+         * Write your code here.
+         */
+
+    }
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(scan.nextLine().trim());
+
+        int[][] a = new int[n][n];
+
+        for (int aRowItr = 0; aRowItr < n; aRowItr++) {
+            String[] aRowItems = scan.nextLine().split(" ");
+
+            for (int aColumnItr = 0; aColumnItr < n; aColumnItr++) {
+                int aItem = Integer.parseInt(aRowItems[aColumnItr].trim());
+                a[aRowItr][aColumnItr] = aItem;
+            }
+        }
+
+        int result = diagonalDifference(a);
+
+        bw.write(String.valueOf(result));
+        bw.newLine();
+
+        bw.close();
+    }
+}
