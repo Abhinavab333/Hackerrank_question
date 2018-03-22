f15a91820fcf4e275e69b6520d9d58763762c2e2
avabhi <32228794+avabhi@users.noreply.github.com>
2018-03-23 02:47:13 +0530

    final answer to this question
    Final Answer

--- DiagonalDiff.java
+++ DiagonalDiff.java
@@ -4,49 +4,61 @@ import java.text.*;
 import java.util.*;
 import java.util.regex.*;
 
- class DiagonalDiff {
+public class DiagonalDiff {
 
-	int n;
-	int a1=n-1;
     /*
      * Complete the diagonalDifference function below.
      */
-     int diagonalDifference(int a[][]) {
+    static int diagonalDifference(int[][] a) {
         /*
          * Write your code here.
          */
-		 int sum1=0;
-	 int sum2=0;
-	 int result;
-		 for(int i=0;i<a.length;i++)
-		 {
-			 sum1=sum1+a[i][i];
-		 }
-		  for(int i=n;i>a[a1].length;i--)
-		 {
-			 sum2=sum2+a[i][i];
-		 }
-		 result=sum1+sum2;
-		 return result;
+		 int sum1=0,sum2=0;
+	 int a1=a.length;
+	 int result,i=0,j=(a1-1);
+	 for( int pi=0;pi<a1;pi++)
+	 {
+		 sum1+=a[pi][pi];
+	 }
+	 //System.out.println(sum1);
+	 
+	while(i<a1)
+	{
+		while(j>=0)
+		{
+			sum2+=a[i][j];
+			break;
+			
+		}
+		j--;
+		i++;
+		}
+	//System.out.println(sum2);
+	 result=Math.abs(sum1-sum2);
+	 return result;
+
     }
 
-    
+    private static final Scanner scan = new Scanner(System.in);
 
-    public static void main(String[] args)  {
-         Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        
 
-        int n = scan.nextInt();
+        int n = Integer.parseInt(scan.nextLine().trim());
 
         int[][] a = new int[n][n];
-		for(int i=0;i<n;i++)
-		{
-			for(int j=0;j<n;j++)
-			{
-				a[i][j]=scan.nextInt();
-			}
-			}
+
+        for (int aRowItr = 0; aRowItr < n; aRowItr++) {
+            String[] aRowItems = scan.nextLine().split(" ");
+
+            for (int aColumnItr = 0; aColumnItr < n; aColumnItr++) {
+                int aItem = Integer.parseInt(aRowItems[aColumnItr].trim());
+                a[aRowItr][aColumnItr] = aItem;
+            }
+        }
+
         int result = diagonalDifference(a);
 
-       System.out.println(result);
+        System.out.println(result);
     }
 }
