2e81e9806690bec39466b7c9136806cc44e64f2a
avabhi <32228794+avabhi@users.noreply.github.com>
2018-03-27 17:19:55 +0530

    entry of phonebook
    adding entries to phonebook

--- Dictionary.java
+++ Dictionary.java
@@ -2,19 +2,24 @@
 import java.util.*;
 import java.io.*;
 
-class Solution{
+class Dictionary{
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
+        Map<String,Integer> PhoneBook=new HashMap<String,Integer>();
         for(int i = 0; i < n; i++){
             String name = in.next();
             int phone = in.nextInt();
             // Write code here
+            PhoneBook.put(name,phone);
+            
         }
-        while(in.hasNext()){
+		
+        /*while(in.hasNext()){
             String s = in.next();
             // Write code here
-        }
+			System.out.println(PhoneBook.toString());
+        }*/
         in.close();
     }
 }
