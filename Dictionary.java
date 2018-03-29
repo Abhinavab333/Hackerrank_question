ddcdc106bca2f5808d70d6abdbadd7aea6f0a4ff
avabhi <32228794+avabhi@users.noreply.github.com>
2018-03-29 20:34:40 +0530

    logic created

--- Dictionary.java
+++ Dictionary.java
@@ -5,9 +5,12 @@ import java.io.*;
 class Dictionary{
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
+		String a[]=new String[100000];
         int n = in.nextInt();
+		int i=0,j=0,c=0;
+		long  m=100000;
         Map<String,Integer> PhoneBook=new HashMap<String,Integer>();
-        for(int i = 0; i < n; i++){
+        for( i = 0; i < n; i++){
             String name = in.next();
             int phone = in.nextInt();
             // Write code here
@@ -15,11 +18,32 @@ class Dictionary{
             
         }
 		
-        /*while(in.hasNext()){
+        while(!in.hasNextInt()){
             String s = in.next();
+			while(i<m)
+			{
+				//System.out.println("a");
+				a[i-1]=s;
+				c+=1;
+				break;
+				
+			}
+			i+=1;
             // Write code here
-			System.out.println(PhoneBook.toString());
-        }*/
+			
+        }
+		
+for(String key:PhoneBook.keySet())
+{
+	for(j=0;j<c;j++)
+	{
+		if(a[j]==key)
+		{
+System.out.println("abc");
+		}
+		}
+		}
+		
         in.close();
     }
 }
